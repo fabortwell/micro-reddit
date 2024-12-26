00ce050e15ef1750074af03c97f3ccce214fe765
Brian Sitati <briansit93@gmail.com>
2024-12-26 15:30:51 +0300

    added logic to User and Post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
+  validates :title, presence: true,
+                   length: { minimum: 3, maximum: 300 }
+  validates :url, presence: true,
+                 format: { with: URI.regexp(%w[http https]), message: "must be a valid URL" }
+  validates :user_id, presence: true
 end
